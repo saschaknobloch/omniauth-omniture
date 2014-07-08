83973b24cd29e249e32a243e7dfbfec0f8e5b6b5
Sascha Knobloch <saschaknobloch.dev@gmail.com>
2014-07-08 13:34:36 +0200

    Add omniture specific options.

--- lib/omniauth/strategies/omniture.rb
+++ lib/omniauth/strategies/omniture.rb
@@ -1,9 +1,28 @@
-require 'omniauth-oauth2'
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
     class Omniture < OmniAuth::Strategies::OAuth2
 
+      option :name, "omniture"
+
+      option :client_options, {
+        site:          "https://api.omniture.com",
+        token_url:     "https://api.omniture.com/token",
+        authorize_url: "https://marketing.adobe.com/authorize"
+      }
+
+      option :authorize_params, {
+        grant_type: "client_credentials"
+      }
+
+      uid {
+        raw_info.client.id
+      }
+
+      def raw_info
+        @raw_info ||= access_token
+      end
     end
   end
 end
